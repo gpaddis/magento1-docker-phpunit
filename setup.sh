a81d14a0b3549ca917b9b379716c4d160dcab62d
Gianpiero Addis <gianpiero.addis@gmail.com>
2019-10-25 13:05:38 +0200

    Add setup script

--- setup.sh
+++ setup.sh
@@ -5,4 +5,23 @@ if [ ! -d "magento" ]; then
     rm -rf magento/.git
 fi
 
+# Detect OS
+case `uname -s` in
+    Darwin) is_macos=yes ;;
+    *) is_macos=no ;;
+esac
+
+searchAndReplace() {
+    local original="$1"
+    local replacement="$2"
+    local file="$3"
+    if [ "$is_macos" = no ]; then
+        sed -i "s%${original}%${replacement}%g" $file
+    elif [ "$is_macos" = yes ]; then
+        sed -i '' "s%${original}%${replacement}%g" $file
+    fi
+}
+
+searchAndReplace "/path/to/magento" "${PWD}/magento"
+
 docker-compose up -d --build
