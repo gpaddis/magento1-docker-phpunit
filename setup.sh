7db3aa5143e422e4f01d99430995b35b8f3fc1b7
Gianpiero Addis <gianpiero.addis@gmail.com>
2019-10-27 14:46:31 +0100

    Link script before tests

--- setup.sh
+++ setup.sh
@@ -29,10 +29,10 @@ if [ ! -f magento/app/etc/local.xml ]; then
     docker exec -it web-magento1-phpunit bash -c "cd app && bash install-magento.sh"
 fi
 
+# Add phpunit-magento to bin
+ls -snf $PWD/phpunit-magento /usr/local/bin/phpunit-magento
+
 # Create and initialize the test database
 docker exec -it mysql-magento1-phpunit bash -c "mysql -uroot -proot -e 'DROP SCHEMA IF EXISTS magento_tests; CREATE SCHEMA magento_tests'"
 echo "Initializing the test database (this can take up to a few minutes)..."
 docker exec -it web-magento1-phpunit bash -c "cd app && ./vendor/bin/phpunit"
-
-# Add phpunit-magento to bin
-ls -snf $PWD/phpunit-magento /usr/local/bin/phpunit-magento
