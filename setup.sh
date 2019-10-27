39b0a342c69f1b57363f88804b18ffc18860ec18
Gianpiero Addis <gianpiero.addis@gmail.com>
2019-10-27 15:45:28 +0100

    Fix ln, add confirmation

--- setup.sh
+++ setup.sh
@@ -30,9 +30,11 @@ if [ ! -f magento/app/etc/local.xml ]; then
 fi
 
 # Add phpunit-magento to bin
-ls -snf $PWD/phpunit-magento /usr/local/bin/phpunit-magento
+ln -snf $PWD/phpunit-magento /usr/local/bin/phpunit-magento
 
 # Create and initialize the test database
 docker exec -it mysql-magento1-phpunit bash -c "mysql -uroot -proot -e 'DROP SCHEMA IF EXISTS magento_tests; CREATE SCHEMA magento_tests'"
 echo "Initializing the test database (this can take up to a few minutes)..."
-docker exec -it web-magento1-phpunit bash -c "cd app && ./vendor/bin/phpunit"
+docker exec -it web-magento1-phpunit bash -c "cd app && ./vendor/bin/phpunit" || {
+    echo "Setup complete. Run phpunit-magento from a module directory to execute the tests!"
+}
